a5a1088816af4ac5a44eafd6fa88c289d7692ac8
A Cripps <adam@monkeez.org>
2015-07-23 21:23:36 +0100

    Cleaning up a lot.

--- _site/wp-content/uploads/sucuri/sucuri-ignorescanning.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=ignorescanning;
-// created_on=1411067181;
-// updated_on=1411067181;
-exit(0);
-?>
--- wp-content/uploads/sucuri/sucuri-trustip.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=trustip;
-// created_on=1417166672;
-// updated_on=1417166672;
-exit(0);
-?>
